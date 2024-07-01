3cb6964c3023224b86771f52f69e16f71c965a8f
Vinicius Peixoto <vinicius.opeixoto@gmail.com>
2024-07-01 00:15:18 -0300

    Tree catalog

--- src/lib/pages/trees.js
+++ src/lib/pages/trees.js
@@ -1,8 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { listTrees } from '../components/api';
+import { Container, ListGroup } from 'react-bootstrap';
 
 const TreesPage = () => {
+    const [treesData, setTreesData] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchTrees = async () => {
+            try{
+                const treesData = await listTrees();
+                console.log('trees data:', treesData)
+                setTreesData(treesData)
+                setLoading(false);
+            } catch (error) {
+                console.error('Error fetching trees information:', error)
+                setLoading(false)
+            }
+        };
+
+        fetchTrees();
+    }, []);
+
     return (
-        <h1>Trees</h1>
+        <Container className='mt-5'>
+            <h2>Trees Catalog</h2>
+            {loading ? (
+                <p>Loading trees information...</p>
+            ) : treesData ? (
+                <ListGroup>
+                    {treesData.map((tree) => (
+                        <ListGroup.Item key={tree.id}>
+                            <h5>{tree.name}</h5>
+                            <p className='fst-italic'>{tree.scientific_name}</p>
+                        </ListGroup.Item>
+                    ))}
+                </ListGroup>
+            ) : (
+                <p>No trees data to show</p>
+            )}
+        </Container>
     );
 };
 
