4715c24b100057fd46ad66f4ce5e4bf1524dd719
Vinicius Peixoto <vinicius.opeixoto@gmail.com>
2024-07-01 15:34:47 -0300

    Block access for anonymous users

--- src/lib/pages/accountPlanted.js
+++ src/lib/pages/accountPlanted.js
@@ -4,6 +4,7 @@ import { Button, Container, Form, ListGroup } from 'react-bootstrap';
 import { formatDateTime } from '../components/dateFormatter';
 
 const AccountPlantedPage = () => {
+    const [user, setUser] = useState(null);
     const [accounts, setAccounts] = useState([]);
     const [account, setAccount] = useState('');
     const [treesData, setTreesData] = useState(null);
@@ -17,6 +18,7 @@ const AccountPlantedPage = () => {
                     throw new Error('Failed to fetch current user')
                 }
                 const userData = await user_response.json();
+                setUser(userData)
                 setAccounts(userData.accounts);
             } catch (error) {
                 console.error('Error fetching current user:', error)
@@ -49,41 +51,52 @@ const AccountPlantedPage = () => {
         setAccount(event.target.value);
     };
 
+    const renderAccountTrees = () => {
+        return (
+            <Container>
+                <Form>
+                    <Form.Group controlId='accountSelect'>
+                        <Form.Label>Select account</Form.Label>
+                        <Form.Control as='select' value={account} onChange={handleAccountChange}>
+                            <option value=''>Select an account...</option>
+                            {accounts.map((acc) => (
+                                <option key={acc.id} value={acc.name}>
+                                    {acc.name}
+                                </option>
+                            ))}
+                        </Form.Control>
+                    </Form.Group>
+                </Form>
+                {loading ? (
+                    <p>Loading trees information...</p>
+                ) : treesData ? (
+                    <ListGroup>
+                        {treesData.map((tree) => (
+                            <ListGroup.Item key={tree.id}>
+                                <h5>{tree.tree.name}</h5>
+                                <p className='fst-italic'>{tree.tree.scientific_name}</p>
+                                <p>Account: {tree.account.name}</p>
+                                <p>Planted at: {formatDateTime(tree.planted_at)}</p>
+                                <p>Age: {tree.age}</p>
+                                <p>Location: ({tree.location[0]}, {tree.location[1]})</p>
+                            </ListGroup.Item>
+                        ))}
+                    </ListGroup>
+                ) : (
+                    <p>No trees data to show</p>
+                )}
+                <Button variant='primary' href='/plant'>Plant a new tree</Button>
+            </Container>
+        )
+    }
     return (
         <Container className='mt-5'>
             <h2>Your Account Planted Trees</h2>
-            <Form>
-                <Form.Group controlId='accountSelect'>
-                    <Form.Label>Select account</Form.Label>
-                    <Form.Control as='select' value={account} onChange={handleAccountChange}>
-                        <option value=''>Select an account...</option>
-                        {accounts.map((acc) => (
-                            <option key={acc.id} value={acc.name}>
-                                {acc.name}
-                            </option>
-                        ))}
-                    </Form.Control>
-                </Form.Group>
-            </Form>
-            {loading ? (
-                <p>Loading trees information...</p>
-            ) : treesData ? (
-                <ListGroup>
-                    {treesData.map((tree) => (
-                        <ListGroup.Item key={tree.id}>
-                            <h5>{tree.tree.name}</h5>
-                            <p className='fst-italic'>{tree.tree.scientific_name}</p>
-                            <p>Account: {tree.account.name}</p>
-                            <p>Planted at: {formatDateTime(tree.planted_at)}</p>
-                            <p>Age: {tree.age}</p>
-                            <p>Location: ({tree.location[0]}, {tree.location[1]})</p>
-                        </ListGroup.Item>
-                    ))}
-                </ListGroup>
+            {user ? (
+                renderAccountTrees()
             ) : (
-                <p>No trees data to show</p>
+                <p>Please login to see your accounts' planted trees' information</p>
             )}
-            <Button variant='primary' href='/plant'>Plant a new tree</Button>
         </Container>
     );
 };
--- src/lib/pages/planted.js
+++ src/lib/pages/planted.js
@@ -1,11 +1,29 @@
 import React, { useEffect, useState } from 'react';
-import { listPlantedOwn } from '../components/api';
+import { getCurrentUser, listPlantedOwn } from '../components/api';
 import { Button, Container, ListGroup } from 'react-bootstrap';
 import { formatDateTime } from '../components/dateFormatter';
 
 const PlantedPage = () => {
     const [treesData, setTreesData] = useState(null);
     const [loading, setLoading] = useState(true);
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const fetchCurrentUser = async () => {
+            try{
+                const user_response = await getCurrentUser();
+                if (!user_response.ok) {
+                    throw new Error('Failed to fetch current user')
+                }
+                const userData = await user_response.json();
+                setUser(userData);
+            } catch (error) {
+                console.error('Error fetching current user:', error)
+            }
+        };
+
+        fetchCurrentUser();
+    }, []);
 
     useEffect(() => {
         const fetchTrees = async () => {
@@ -22,30 +40,43 @@ const PlantedPage = () => {
         fetchTrees();
     }, []);
 
+    const renderPlantedTrees = () => {
+        return (
+            <Container className='mt-5'>
+                {loading ? (
+                    <p>Loading trees information...</p>
+                ) : treesData ? (
+                    <ListGroup>
+                        {treesData.map((tree) => (
+                            <ListGroup.Item key={tree.id}>
+                                <h5>{tree.tree.name}</h5>
+                                <p className='fst-italic'>{tree.tree.scientific_name}</p>
+                                <p>Account: {tree.account.name}</p>
+                                <p>Planted at: {formatDateTime(tree.planted_at)}</p>
+                                <p>Age: {tree.age}</p>
+                                <p>Location: ({tree.location[0]}, {tree.location[1]})</p>
+                            </ListGroup.Item>
+                        ))}
+                    </ListGroup>
+                ) : (
+                    <p>No trees data to show</p>
+                )}
+                <Button variant='primary' href='/plant'>Plant a new tree</Button>
+            </Container>
+        )
+    }
+
     return (
         <Container className='mt-5'>
             <h2>Your Planted Trees</h2>
-            {loading ? (
-                <p>Loading trees information...</p>
-            ) : treesData ? (
-                <ListGroup>
-                    {treesData.map((tree) => (
-                        <ListGroup.Item key={tree.id}>
-                            <h5>{tree.tree.name}</h5>
-                            <p className='fst-italic'>{tree.tree.scientific_name}</p>
-                            <p>Account: {tree.account.name}</p>
-                            <p>Planted at: {formatDateTime(tree.planted_at)}</p>
-                            <p>Age: {tree.age}</p>
-                            <p>Location: ({tree.location[0]}, {tree.location[1]})</p>
-                        </ListGroup.Item>
-                    ))}
-                </ListGroup>
+            {user ? (
+                renderPlantedTrees()
             ) : (
-                <p>No trees data to show</p>
+                <p>Please login to see your planted trees' information</p>
             )}
-            <Button variant='primary' href='/plant'>Plant a new tree</Button>
         </Container>
-    );
+    )
+    
 };
 
 export default PlantedPage;
